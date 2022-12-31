0882b633e1e4396a8548384a32dfcab08d6e951b
Suman Islam <sumanislam1416@gmail.com>
2022-12-31 14:56:47 +0600

    update cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const api = require('./routes/api');
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'https://nasamissioncontrol.vercel.app'],
+	})
+);
 //  {
 //   origin: 'http://localhost:3000'
 // }
